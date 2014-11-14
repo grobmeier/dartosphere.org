b1b9d2c6cd13e7f651644c845461fb5505309df5
Christian Grobmeier <cg@grobmeier.de>
2014-11-14 14:47:30 +0100

    added releases page

---
+++ _plugins/releases.rb
@@ -0,0 +1,39 @@
+module Jekyll
+  class ReleasesBlock < Liquid::Block
+    def initialize(tag_name, params, tokens)
+      super
+      @params = params
+    end
+
+    def render(context)
+    	html = super
+    	params = @params
+
+    	params.strip!
+      site = context.registers[:site]
+
+      i = 0
+
+      html = "<ul>"
+      site.data["generated"]["releases"].reverse.each do |release|
+
+        html = html + "<li>"
+        html = html + "<a href=\"" + release["id"] + "\">"
+        html = html + release["package_name"] + ": " + release["package_version"]
+        html = html + "</a> - "
+        html = html + "<i>" + release["author"] +  "</i>"
+        html = html + "</li>"
+
+        i = i + 1
+        if i >= params.to_i
+          break
+        end
+      end
+      html = html + "</ul>"
+
+      html.strip
+    end
+  end
+
+  Liquid::Template.register_tag('releases', ReleasesBlock)
+end
